38f7ab6a1040ddeb4a4d18f2bd4a4e39be12eec4
Bellangelo <aggelosbellos7@gmail.com>
2024-08-02 00:29:22 +0300

    Initiate dummy test

---
+++ tests/MainTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MainTest extends TestCase
+{
+    public function test_happy_path(): void
+    {
+        $this->assertTrue(true);
+    }
+}
