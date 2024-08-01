d5f580df3c2d5248b8f2e34cb7e015b893cc5ef1
Bellangelo <aggelosbellos7@gmail.com>
2024-08-02 01:20:25 +0300

    Refactor tests

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -10,34 +10,23 @@ class MainTest extends TestCase
 {
     public function testHappyPath(): void
     {
-        $expectedTotalErrors = 4;
+        $expectedTotalErrors = 12;
         $expectedErrorsPerFile = [
+            'file_exists_but_path_is_relative.php' => [
+                'errors' => 4,
+                'lines' => [5, 6, 7, 8]
+            ],
             'included_file_does_not_exist.php' => [
                 'errors' => 4,
-                'messages' => [
-                    [
-                        'message' => 'Included or required file "a_file_that_does_not_exist.php" does not exist.',
-                        'line' => 5,
-                        'ignorable' => true
-                    ],
-                    [
-                        'message' => 'Included or required file "a_file_that_does_not_exist_once.php" does not exist.',
-                        'line' => 6,
-                        'ignorable' => true
-                    ],
-                    [
-                        'message' => 'Included or required file "a_file_that_does_not_exist.php" does not exist.',
-                        'line' => 7,
-                        'ignorable' => true
-                    ],
-                    [
-                        'message' => 'Included or required file "a_file_that_does_not_exist_once.php" does not exist.',
-                        'line' => 8,
-                        'ignorable' => true
-                    ],
-                ]
+                'lines' => [5, 6, 7, 8]
+            ],
+            'included_file_does_not_exist_using_class_const.php' => [
+                'errors' => 4,
+                'lines' => [7, 8, 9, 10]
             ],
         ];
+        $errorStartsWith = 'Included or required file "';
+        $errorEndsWith = '" does not exist.';
 
         $output = [];
         exec('php vendor/bin/phpstan analyse -c tests/phpstan-testing.neon --error-format=json', $output);
@@ -51,8 +40,16 @@ class MainTest extends TestCase
             $basename = basename($filename);
 
             $this->assertArrayHasKey($basename, $expectedErrorsPerFile);
-            $this->assertSame($expectedErrorsPerFile[$basename]['errors'], $errors['errors']);
-            $this->assertSame($expectedErrorsPerFile[$basename]['messages'], $errors['messages']);
+
+            $expectedError = $expectedErrorsPerFile[$basename];
+            $this->assertSame($expectedError['errors'], $errors['errors']);
+
+            foreach ($errors['messages'] as $index => $error) {
+                $this->assertStringStartsWith($errorStartsWith, $error['message']);
+                $this->assertStringEndsWith($errorEndsWith, $error['message']);
+
+                $this->assertSame($expectedError['lines'][$index], $error['line']);
+            }
         }
     }
 }
---
+++ tests/files/file_exists_but_path_is_relative.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+include 'include_me_to_prove_you_work.txt';
+include_once 'include_me_to_prove_you_work.txt';
+require 'include_me_to_prove_you_work.txt';
+require_once 'include_me_to_prove_you_work.txt';
