97e0d5bf838bb87a490cf2e5c9d46bb2c8310900
Bellangelo <aggelosbellos7@gmail.com>
2024-08-02 01:02:19 +0300

    Test that it can find paths that use __DIR__

---
+++ tests/files/file_exists_using_DIR.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+include __DIR__ . '/include_me_to_prove_you_work.txt';
+include_once __DIR__ . '/include_me_to_prove_you_work.txt';
+require __DIR__ . '/include_me_to_prove_you_work.txt';
+require_once __DIR__ . '/include_me_to_prove_you_work.txt';
