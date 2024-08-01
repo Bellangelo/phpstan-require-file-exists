23a356011a0398d83b4afaac2aa371f3320a6d04
Bellangelo <aggelosbellos7@gmail.com>
2024-08-02 02:25:12 +0300

    Function must stay there for readability

--- tests/files/included_file_does_not_exist_but_uses_a_function.php
+++ tests/files/included_file_does_not_exist_but_uses_a_function.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
+// phpcs:disable
 function getFileThatDoesNotExist(): string
 {
     return 'a_file_that_does_not_exist.txt';
 }
 
+// phpcs:enable
 include __DIR__ . '/' . getFileThatDoesNotExist();
 include_once __DIR__ . '/' . getFileThatDoesNotExist();
 require __DIR__ . '/' . getFileThatDoesNotExist();
